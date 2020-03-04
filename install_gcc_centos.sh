7ad6f84795c9b3fbff15bf508fb5af0c6a2e3cb1
Jie <37380896+jren73@users.noreply.github.com>
2020-03-03 23:10:41 -0800

    Update install_gcc_centos.sh

--- install_gcc_centos.sh
+++ install_gcc_centos.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+unset PATH
+unset LD_LIBRARY_PATH
+export PATH=/usr/bin:/home/cc/.local/bin:/home/cc/bin:
+export LD_LIBRARY_PATH=/home/cc/mvapich2-2.2/lib/.libs/:
+
 mkdir gcc-5.4.0-build
 cd gcc-5.4.0-build
 ../gcc-5.4.0/configure --enable-languages=c,c++,fortran --disable-multilib
