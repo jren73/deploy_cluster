ebf0186d118ace534f1e0ab055a92097f6cbe0fc
Jie <37380896+jren73@users.noreply.github.com>
2020-03-03 23:18:49 -0800

    Update install_gcc_centos.sh

--- install_gcc_centos.sh
+++ install_gcc_centos.sh
@@ -5,6 +5,10 @@ unset LD_LIBRARY_PATH
 export PATH=/usr/bin:/home/cc/.local/bin:/home/cc/bin:
 export LD_LIBRARY_PATH=/home/cc/mvapich2-2.2/lib/.libs/:
 
+curl https://ftp.gnu.org/gnu/gcc/gcc-5.4.0/gcc-5.4.0.tar.bz2 -O
+tar jxvf gcc-5.4.0.tar.bz2
+sudo yum install gmp-devel mpfr-devel libmpc-devel
+
 mkdir gcc-5.4.0-build
 cd gcc-5.4.0-build
 ../gcc-5.4.0/configure --enable-languages=c,c++,fortran --disable-multilib
